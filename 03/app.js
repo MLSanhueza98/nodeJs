39871aace3f4aa32ba4430c24d03c156eacdbe7d
Mario Labbé <mario.ignacio19@gmail.com>
2023-11-24 13:58:24 -0300

    clase-3/get-post-patch-method

---
+++ 03/app.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const crypto = require('node:crypto')
+const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./movie')
+
+const app = express()
+app.use(express.json())
+
+app.disable('x-powered-by') // deshabilitr el header X-Powered-By: Express
+
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLocaleLowerCase() === genre.toLocaleLowerCase()) // sensitiveCase
+    )
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => { // path-to-regexp ()
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie Not Found' })
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+  movies.push(newMovie)
+  res.status(201).json(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+const PORT = process.env.port ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
