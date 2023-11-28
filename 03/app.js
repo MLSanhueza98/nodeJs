e874751d1b8879563371d888b8cc9d1a0ca4dc12
Mario Labbé <mario.ignacio19@gmail.com>
2023-11-28 11:38:09 -0300

    CORS: problemas, soluciones

--- 03/app.js
+++ 03/app.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const crypto = require('node:crypto')
 const movies = require('./movies.json')
+const cors = require('cors')
 const { validateMovie, validatePartialMovie } = require('./movie')
 
 const app = express()
 app.use(express.json())
 
 app.disable('x-powered-by') // deshabilitr el header X-Powered-By: Express
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:1234',
+      'http://localhost:8080'
+    ]
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
 
 app.get('/movies', (req, res) => {
   const { genre } = req.query
@@ -39,6 +56,18 @@ app.post('/movies', (req, res) => {
   }
   movies.push(newMovie)
   res.status(201).json(newMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie Not Found' })
+  }
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie Deleted' })
 })
 
 app.patch('/movies/:id', (req, res) => {
